b54169797acd66e89306d98633cc478cf3c97674
Nicolas ZERMATI <nicoolas25@gmail.com>
2011-12-19 15:55:29 +0100

    Update README and template.

--- lib/guard/i18n.rb
+++ lib/guard/i18n.rb
@@ -6,39 +6,7 @@ require 'yaml'
 module Guard
   class I18n < Guard
 
-    # Initialize a Guard.
-    # @param [Array<Guard::Watcher>] watchers the Guard file watchers
-    # @param [Hash] options the custom Guard options
-    def initialize(watchers = [], options = {})
-      super
-    end
-
-    # Call once when Guard starts. Please override initialize method to init stuff.
-    # @raise [:task_has_failed] when start has failed
-    def start
-    end
-
-    # Called when `stop|quit|exit|s|q|e + enter` is pressed (when Guard quits).
-    # @raise [:task_has_failed] when stop has failed
-    def stop
-    end
-
-    # Called when `reload|r|z + enter` is pressed.
-    # This method should be mainly used for "reload" (really!) actions like reloading passenger/spork/bundler/...
-    # @raise [:task_has_failed] when reload has failed
-    def reload
-    end
-
-    # Called when just `enter` is pressed
-    # This method should be principally used for long action like running all specs/tests/...
-    # @raise [:task_has_failed] when run_all has failed
-    def run_all
-    end
-
-    # Called on file(s) modifications that the Guard watches.
-    # @param [Array<String>] paths the changes files or paths
-    # @raise [:task_has_failed] when run_on_change has failed
-    def run_on_change(paths)
+   def run_on_change(paths)
       target = Pathname.pwd.join('app', 'assets', 'javascripts', 'i18n')
       Dir::mkdir(target) unless File.directory?(target)
 
@@ -55,13 +23,7 @@ module Guard
           f.puts "var locale = {}; locale.t = #{content["javascript"].to_json};"
         end
       end
-    end
-
-    # Called on file(s) deletions that the Guard watches.
-    # @param [Array<String>] paths the deleted files or paths
-    # @raise [:task_has_failed] when run_on_change has failed
-    def run_on_deletion(paths)
-    end
+   end
 
   end
 end

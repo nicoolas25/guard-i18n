130d91948cea2b6a8658c13b0c4c1b428dd2a50e
Nicolas ZERMATI <nicoolas25@gmail.com>
2011-12-20 14:19:23 +0100

    Updating for dot notation and assets pipeline usage.

--- lib/guard/i18n.rb
+++ lib/guard/i18n.rb
@@ -6,10 +6,27 @@ require 'yaml'
 module Guard
   class I18n < Guard
 
-   def run_on_change(paths)
-      target = Pathname.pwd.join('app', 'assets', 'javascripts', 'i18n')
-      Dir::mkdir(target) unless File.directory?(target)
+    TARGET_DIR = Pathname.pwd.join('app', 'assets', 'javascripts', 'i18n')
 
+    COFFEE_TPL = <<-EOS
+locale = {}
+locale.h = `%s`
+locale.t = (name) ->
+  h_result = this.h
+  for n in name.split(".")
+    if n of h_result
+      h_result = h_result[n]
+    else
+      return "missing i18n : " + name
+  return h_result
+root = exports ? this
+root.locale = locale
+      EOS
+
+    def run_on_change(paths)
+      Dir::mkdir(TARGET_DIR) unless File.directory?(TARGET_DIR)
+
+      locales = Rails.root.join('config', 'locales', '**', '*.yml')
       paths.each do |locale_path|
         filename = File.basename(locale_path, ".yml")
         input = File.new(locale_path, 'r')
@@ -19,11 +36,10 @@ module Guard
         name = locale.keys[0]
         content = locale[name]
         next unless content.key?("javascript")
-        File.open(target + "#{filename}.js", "w") do |f|
-          f.puts "var locale = {}; locale.t = #{content["javascript"].to_json};"
+        File.open(TARGET_DIR + "#{filename}.js.coffee", "w") do |f|
+          f.puts COFFEE_TPL % content["javascript"].to_json
         end
       end
-   end
-
+    end
   end
 end
